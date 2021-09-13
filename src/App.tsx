889f19749599e6b3b663d29860e42c47b0104d01
Hedyjing <374884275@qq.com>
2021-09-14 00:27:49 +0800

    XSS example

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,19 @@ import './App.css';
 import cartoons from './mockdata/cartoon.json'
 import Cartoon from './components/Cartoon'
 
+// XSS攻击演示, 第一种react进行了防范, 把html内容转为了字符串, 但第二种不能react防范不了
+const html = "<img onerror='alert(\"Hacked!\")' src='invalid-image' />"
+const jsHacked = "javascript: alert('Hacked!')";
+
 function App() {
   return (
-    <ul>
-      {cartoons.map(c => <Cartoon id={c.id} name={c.name} email={c.email}/>)}
-    </ul>
+    <>
+    <div>{html}</div> 
+    <a href={jsHacked} >点击</a>
+      <ul>
+        {cartoons.map(c => <Cartoon id={c.id} name={c.name} email={c.email} />)}
+      </ul>
+    </>
   )
 }
 
