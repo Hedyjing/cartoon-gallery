c644abd682a18f111a3bb725bf3bb5e0c28281f7
LAPTOP-2T7DJO2V\HanJing <jingha2@cisco.com>
2021-09-14 14:22:55 +0800

    this指向问题

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -14,11 +14,17 @@ class ShoppingCart extends React.Component<Props, State> {
     this.state = {
       isOpen: false,
     };
+    // this.handleClick = this.handleClick.bind(this);  // 给方法绑定类对象的this防止方法内部的this指向错误
+  }
+  // 对象方法中this， 函数为普通函数时， this指向函数本身， 为箭头函数时指向父级作用域；
+  // 两个方案： 1， 用箭头函数， 2， 在构造函数中用bind给方法绑定类的this；
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    this.setState({ isOpen: !this.state.isOpen })
   }
   render() {
     return (
       <div className={styles.cartContainer}>
-        <button className={styles.button} onClick={() => this.setState({ isOpen: !this.state.isOpen })}>购物车2（件）</button>
+        <button className={styles.button} onClick={this.handleClick}>购物车2（件）</button>
         <div className={styles.cartDropDown}
           style={{
             display: this.state.isOpen ? 'block' : 'none'
