63f037a0e98b1d5ddc7dd413abc8158b63f37dcc
LAPTOP-2T7DJO2V\HanJing <jingha2@cisco.com>
2021-09-14 11:02:49 +0800

    Add assets and fonts in css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 import cartoons from './mockdata/cartoon.json'
 import Cartoon from './components/Cartoon'
 import styles from './App.module.css';
@@ -8,6 +8,10 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} alt="logo" className={styles.appLogo} />
+        <h1>Cartoon Gallery Card Show</h1>
+      </div>
       <div className={styles.robotList}>
         {cartoons.map(c => <Cartoon id={c.id} name={c.name} email={c.email} />)}
       </div>
