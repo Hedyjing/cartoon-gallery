152589df7ca28d89506472b0f56c081bacd5b3a6
LAPTOP-2T7DJO2V\HanJing <jingha2@cisco.com>
2021-09-14 16:12:48 +0800

    Reform class component and async data

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,37 @@ import Cartoon from './components/Cartoon'
 import styles from './App.module.css';
 import ShoppingCart from './components/ShoppingCart';
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} alt="logo" className={styles.appLogo} />
-        <h1>Cartoon Gallery Card Show</h1>
+interface Props { }
+interface State {
+  cartoonGallery: any[]
+}
+
+class App extends React.Component<Props, State>{
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      cartoonGallery: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => this.setState({ cartoonGallery: data }));
+  }
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} alt="logo" className={styles.appLogo} />
+          <h1>Cartoon Gallery Card Show</h1>
+        </div>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.cartoonGallery.map(c => <Cartoon id={c.id} name={c.name} email={c.email} />)}
+        </div>
       </div>
-      <ShoppingCart/>
-      <div className={styles.robotList}>
-        {cartoons.map(c => <Cartoon id={c.id} name={c.name} email={c.email} />)}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
