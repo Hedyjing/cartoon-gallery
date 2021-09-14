f20e6b460c50eecd1e1c749d7690adae371ca95c
LAPTOP-2T7DJO2V\HanJing <jingha2@cisco.com>
2021-09-14 10:25:31 +0800

    CSS模块化引入(JSS)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import cartoons from './mockdata/cartoon.json'
 import Cartoon from './components/Cartoon'
+import styles from './App.module.css';
 
-// XSS攻击演示, 第一种react进行了防范, 把html内容转为了字符串, 但第二种不能react防范不了
-const html = "<img onerror='alert(\"Hacked!\")' src='invalid-image' />"
-const jsHacked = "javascript: alert('Hacked!')";
 
 function App() {
   return (
-    <>
-    <div>{html}</div> 
-    <a href={jsHacked} >点击</a>
-      <ul>
+    <div className={styles.app}>
+      <div className={styles.robotList}>
         {cartoons.map(c => <Cartoon id={c.id} name={c.name} email={c.email} />)}
-      </ul>
-    </>
+      </div>
+    </div>
   )
 }
 
