bb4db382a4e3c7bd8b01ff380a8ba715280046f8
Hedyjing <374884275@qq.com>
2021-09-15 23:04:24 +0800

    Use 'useContext' Hooks to transfer data

--- src/components/Cartoon.tsx
+++ src/components/Cartoon.tsx
@@ -1,5 +1,5 @@
 import { appendFile } from 'fs';
-import React from 'react';
+import React, { useContext } from 'react';
 import { appContext } from '..';
 import styles from './Cartoon.module.css';
 
@@ -10,18 +10,15 @@ interface CartoonProps {
 }
 
 const Cartoon: React.FC<CartoonProps> = ({ id, name, email }) => {
-
+  const value = useContext(appContext);
   return (
-    <appContext.Consumer>
-      {
-        (value) => <div className={styles.cardContainer}>
-          <img src={`https://robohash.org/${name}`} alt="cartoon" />
-          <h2>{name}</h2>
-          <p>{email}</p>
-          <p>作者: {value.username}</p>
-        </div>
-      }
-    </appContext.Consumer>)
+    <div className={styles.cardContainer}>
+      <img src={`https://robohash.org/${name}`} alt="cartoon" />
+      <h2>{name}</h2>
+      <p>{email}</p>
+      <p>作者: {value.username}</p>
+    </div>
+  )
 }
 
 export default Cartoon;
