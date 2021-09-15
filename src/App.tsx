951f2b0eb6c5931aee50402d2cac585ad3a7deed
LAPTOP-2T7DJO2V\HanJing <jingha2@cisco.com>
2021-09-15 17:37:33 +0800

    利用hooks构建第一个函数式组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './assets/images/logo.svg';
 import cartoons from './mockdata/cartoon.json'
 import Cartoon from './components/Cartoon'
@@ -11,40 +11,9 @@ interface State {
   count: number
 }
 
-class App extends React.Component<Props, State>{
-  /** 
-   * 生命周期第一阶段： 初始化
-   * 初始化组件 state
-  */
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      cartoonGallery: [],
-      count: 0
-    };
-  }
-  // 在组件创建好dom元素以后，挂载进页面的时候调用
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(data => this.setState({ cartoonGallery: data }));
-  }
-
-  /**
-   * 生命周期第二阶段： 更新
-   * 
-   */
-  // 组件更新后调用
-  componentDidUpdate(){}
-  /**
-   * 生命周期第三阶段： 销毁 
-   * 组件销毁后调用
-   * 可以当作析构函数 destructor来使用
-   */
-  componentWillUnmount() {}
-
-
-  render() {
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+  
     return (
       <div className={styles.app}>
         <div className={styles.appHeader}>
@@ -52,21 +21,15 @@ class App extends React.Component<Props, State>{
           <h1>Cartoon Gallery Card Show</h1>
         </div>
         <button onClick={() => {
-          // setState异步更新， 同步执行， setState中第一个参数可以传入对象来设置state，也可以传入一个回调函数来对上一次state的状态进行处理
-          // setState的第二个参数用来同步setState的异步操作的
-          this.setState((preState, preProps) => {return { count: preState.count + 1}}, () => {console.log("count: ", this.state.count);})
-          this.setState((preState, preProps) => {return { count: preState.count + 1}}, () => {console.log("count: ", this.state.count);})
-          //this.setState({ count: this.state.count + 1}, () => {console.log("count: ", this.state.count);});
-          
+          setCount(count + 1);
         }}>Click</button>
-        <span>count: {this.state.count}</span>
+        <span>count: {count}</span>
         <ShoppingCart />
-        <div className={styles.robotList}>
+        {/* <div className={styles.robotList}>
           {this.state.cartoonGallery.map(c => <Cartoon id={c.id} name={c.name} email={c.email} />)}
-        </div>
+        </div> */}
       </div>
     )
-  }
 }
 
 export default App;
